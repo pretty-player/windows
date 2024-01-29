2195e5f12db69a3b1c92f064d046aec9f7cdc7a2
Silambarasan P <123256821+pretty-player@users.noreply.github.com>
2024-01-29 06:52:41 -0800

    Add files via upload
    you want to installed python3.
    C:\Users\username\Desktop>python matrix.py
    
    Enjoy....

---
+++ matrix.py
@@ -0,0 +1,105 @@
+#colorama Module Required
+from os import system
+
+system("pip install colorama")
+
+from colorama import Fore,init
+from random import choice
+from string import printable
+red,blue,green,yellow=Fore.RED,Fore.BLUE,Fore.GREEN,Fore.YELLOW
+tup=(red,blue,green)
+l=printable
+from time import sleep as slp
+init()
+color=yellow
+print("You want to stop it press Ctrl + c")
+slp(4)
+color=green
+while True:
+    print(color)
+    print(choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l),
+    choice(l))
+    slp(0.3)
+    color=choice(tup)
